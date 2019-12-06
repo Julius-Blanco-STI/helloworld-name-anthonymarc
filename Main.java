827b309c9173b1dc28b0bbc9ed19f7e699c0bab7
anthonymarc <58576083+anthonymarc@users.noreply.github.com>
2019-12-06 08:37:16 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String name="Anthony Marc I. Rosario";
+    System.out.println("Hello World " + name);
   }
 
 }
