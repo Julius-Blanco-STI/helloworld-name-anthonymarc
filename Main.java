82500180e88802b4afbe88246f4b964b2d42705e
anthonymarc <58576083+anthonymarc@users.noreply.github.com>
2019-12-06 09:20:07 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,11 +5,28 @@
  */
 package main;
 
+import java.util.*;
+
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
     String name="Anthony Marc I. Rosario";
-    System.out.println("Hello World " + name);
+    System.out.println("Hello World!");
+    System.out.println("What's your name?");
+    name = scn.nextLine();
+    
+    System.out.println("How old are you?");
+    int age=scn.nextInt();
+    
+    System.out.println("What's your address?");
+    String address = scn.nextLine();
+    address = scn.nextLine();
+         
+    System.out.println("Hi " + name);
+    System.out.println("You are " + age + "years old");
+    System.out.println("You live at " + address);
   }
 
 }
